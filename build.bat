086678477affbdcffac87cde462e755306d57816
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2018-10-14 19:32:57 -0500

    Fix upvote/downvote arrows to use better graphics

--- build.bat
+++ build.bat
@@ -1,8 +1,4 @@
-set FIREFOX="HNE-firefox.zip" 
-set CHROMES="HNE-chrome.zip" 
 
-del "%FIREFOX%"
-del "%CHROMES%"
+del "HNE.zip"
 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "%FIREFOX%" * -r -xr!.git -xr!*screenshots* -xr!build.bat -xr!*notes* -xr!js/jquery-3.2.1.js -xr!%FIREFOX% -xr!%CHROMES%
-"C:\Program Files\7-Zip\7z.exe" a -tzip "%CHROMES%" * -r -xr!.git -xr!*screenshots* -xr!build.bat -xr!*notes* -xr!js/jquery-3.2.1.js -xr!%FIREFOX% -xr!%CHROMES%
+"C:\Program Files\7-Zip\7z.exe" a -tzip "HNE.zip" * -r -xr!.git -xr!*screenshots* -xr!build.bat -xr!*notes* -xr!js/jquery-3.2.1.js -xr!HNE.zip
